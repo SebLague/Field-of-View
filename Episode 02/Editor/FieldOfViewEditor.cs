cd7ef1d355f27f24057ae9be8a42fbe14cf6e977
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2015-12-28 20:23:36 +0200

    Episode 02

---
+++ Episode 02/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor (typeof (FieldOfView))]
+public class FieldOfViewEditor : Editor {
+
+	void OnSceneGUI() {
+		FieldOfView fow = (FieldOfView)target;
+		Handles.color = Color.white;
+		Handles.DrawWireArc (fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+		Vector3 viewAngleA = fow.DirFromAngle (-fow.viewAngle / 2, false);
+		Vector3 viewAngleB = fow.DirFromAngle (fow.viewAngle / 2, false);
+
+		Handles.DrawLine (fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+		Handles.DrawLine (fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+
+		Handles.color = Color.red;
+		foreach (Transform visibleTarget in fow.visibleTargets) {
+			Handles.DrawLine (fow.transform.position, visibleTarget.position);
+		}
+	}
+
+}
